f0d959a0bfd7cdd0a1fa7ae42d2c32a4ed6205d8
Denis Keksel <dk0104.dk@gmail.com>
2015-08-04 12:49:35 +0200

    CBK Abstract base class

--- ViewModel/TViewPortofolioViewModel.cs
+++ ViewModel/TViewPortofolioViewModel.cs
@@ -9,27 +9,16 @@
 
 namespace ViewModel
 {
-    using System.Collections.Generic;
     using System.Collections.ObjectModel;
-    using System.ComponentModel;
     using System.Linq;
-    using System.Runtime.CompilerServices;
 
     using Model;
-
-    using ViewModel.Annotations;
 
     /// <summary>
     /// Denis Keksel
     /// </summary>
-    public sealed class TViewPortofolioViewModel : INotifyPropertyChanged,IElementViewModel
+    public sealed class TViewPortofolioViewModel : ElementViewModel
     {
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        private bool isSelected;
-
-        private bool isExpanded;
-
         private Portofolio portofolio;
 
         //---------------------------------------------------------------------
@@ -49,74 +38,14 @@ namespace ViewModel
             this.Parent = null;
             this.portofolio = p;
             this.Name = p.ToString();
-            this.Children = new ReadOnlyCollection<IElementViewModel>((from pgroup in this.portofolio.ProductGroups
-                                                                       select new TViewProductGroupViewModel(pgroup, this)).ToList<IElementViewModel>());
+            this.Children = new ReadOnlyCollection<ElementViewModel>((from pgroup in this.portofolio.ProductGroups
+                                                                       select new TViewProductGroupViewModel(pgroup, this)).ToList<ElementViewModel>());
         }
 
         //---------------------------------------------------------------------
         #endregion
         //---------------------------------------------------------------------
-
-        //---------------------------------------------------------------------
-        #region [Properties]
-        //---------------------------------------------------------------------
-        
-        public IElementViewModel Parent { get; private set; }
-
-        public IEnumerable<IElementViewModel> Children { get; private set; }
-
-        public string Name { get; private set; }
-
-        public bool IsSelected
-        {
-            get
-            {
-                return this.isSelected;
-            }
-            set
-            {
-                this.isSelected = value;
-                this.OnPropertyChanged();
-            }
-        }
-
-        public bool IsExpanded
-        {
-            get
-            {
-                return this.isExpanded;
-            }
-            set
-            {
-                this.isExpanded = value;
-                this.OnPropertyChanged();
-            }
-        }
-
-      
-
-        //---------------------------------------------------------------------
-        #endregion
-        //---------------------------------------------------------------------
-
-        //---------------------------------------------------------------------
-        #region [Methods] 
-        //---------------------------------------------------------------------
-
-        [NotifyPropertyChangedInvocator]
-        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            var handler = this.PropertyChanged;
-            if (handler != null)
-            {
-                handler(this, new PropertyChangedEventArgs(propertyName));
-            }
-        }
 
        
-
-        //---------------------------------------------------------------------
-        #endregion
-        //---------------------------------------------------------------------
     }
 }
--- ViewModel/TViewVersionViewModel.cs
+++ ViewModel/TViewVersionViewModel.cs
@@ -22,30 +22,14 @@ namespace ViewModel
     /// <summary>
     /// Version view Model
     /// </summary>
-    public sealed class TViewVersionViewModel:INotifyPropertyChanged,IElementViewModel 
+    public sealed class TViewVersionViewModel:ElementViewModel 
     {
         //---------------------------------------------------------------------
         #region [Fields]
         //---------------------------------------------------------------------
-        
-        
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        private bool isExpanded;
-
-        private bool isSelected;
-
+   
         private Version version;
-
-        public TViewVersionViewModel(Version version, IElementViewModel parent)
-        {
-            this.version = version;
-            this.Parent = parent;
-            this.Name = version.ToString();
-            this.Children = new ReadOnlyCollection<IElementViewModel>((from feature in this.version.Features
-                                                                       select new FeatureViewModel(feature, this)).ToList<IElementViewModel>());
-        }
-
+        
         //---------------------------------------------------------------------
         #endregion
         //---------------------------------------------------------------------
@@ -54,68 +38,19 @@ namespace ViewModel
         #region [Constructors]
         //---------------------------------------------------------------------
 
-        //---------------------------------------------------------------------
-        #endregion
-        //---------------------------------------------------------------------
-
-        //---------------------------------------------------------------------
-        #region [Properties]
-
-        public IElementViewModel Parent { get; private set; }
-
-        public string Name { get; private set; }
-
-        public IEnumerable<IElementViewModel> Children { get; private set; }
-
-        public bool IsSelected
+        public TViewVersionViewModel(Version version, ElementViewModel parent)
         {
-            get
-            {
-                return this.isSelected;
-            }
-            set
-            {
-                this.isSelected = value;
-                this.OnPropertyChanged();
-            }
+            this.version = version;
+            this.Parent = parent;
+            this.Name = version.ToString();
+            this.Children = new ReadOnlyCollection<ElementViewModel>((from feature in this.version.Features
+                                                                       select new TViewFeatureViewModel(feature, this)).ToList<ElementViewModel>());
         }
-
-        public bool IsExpanded
-        {
-            get
-            {
-                return this.isExpanded;
-            }
-            set
-            {
-                this.isExpanded = value;
-                this.OnPropertyChanged();
-            }
-        }
-
-        //---------------------------------------------------------------------
 
         //---------------------------------------------------------------------
         #endregion
         //---------------------------------------------------------------------
 
-        //---------------------------------------------------------------------
-        #region [Methods]
-
-        [NotifyPropertyChangedInvocator]
-        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            var handler = this.PropertyChanged;
-            if (handler != null)
-            {
-                handler(this, new PropertyChangedEventArgs(propertyName));
-            }
-        }
-
-        //---------------------------------------------------------------------
-
-        //---------------------------------------------------------------------
-        #endregion
-        //---------------------------------------------------------------------
+       
     }
 }
